7dbceb85a3b1857db196763b03595eab7a25640b
EntityBug <FMN_TZX@outlook.com>
2022-06-18 11:54:25 +0800

    fix: 修复commit导致的错误

--- src/Notepad/Notepad.h
+++ src/Notepad/Notepad.h
@@ -1,63 +1,3 @@
-<<<<<<< Updated upstream:Notepad.h
-#ifndef NOTEPAD_H
-#define NOTEPAD_H
-#include "strings.h"
-
-#include <QMainWindow>
-#include <fstream>
-#include <iostream>
-#include <QFont>
-#include <QFileSystemModel>
-
-QT_BEGIN_NAMESPACE
-namespace Ui { class Notepad; }
-QT_END_NAMESPACE
-
-using namespace std;
-
-class Notepad : public QWidget
-{
-    Q_OBJECT
-public slots:
-    void openFile();
-    void saveAs();
-    void openFolder();
-    void openSetting();
-    void about();
-    void find();
-    void onClickTreeView(const QModelIndex &index);
-protected:
-    void init();
-    static void splitString(const std::string&, std::vector<std::string>&, const std::string&);
-    bool saveFile();
-    string path;
-    string file;
-    fstream fs;
-    struct Config {
-        bool autoSave;
-        bool treeView;
-        QString font;
-        bool chinese;
-    };
-    Config cfg;
-    QFileSystemModel * model;
-
-public:
-    explicit Notepad(QWidget * parent = nullptr);
-
-    ~Notepad() override;
-    QString getFontCfg() const;
-    Text * T{};
-
-private:
-    Ui::Notepad *ui;
-
-protected:
-    void closeEvent(QCloseEvent * event) override;
-
-};
-#endif // NOTEPAD_H
-=======
 #ifndef NOTEPAD_H
 #define NOTEPAD_H
 #include "src/Strings/Strings.h"
@@ -116,4 +56,3 @@ protected:
 
 };
 #endif // NOTEPAD_H
->>>>>>> Stashed changes:src/Notepad/Notepad.h
